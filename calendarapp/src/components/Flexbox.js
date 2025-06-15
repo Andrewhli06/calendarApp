4eeebdc2a8cab8f598d2d03472fb32de3dde49b3
jacebaek1009 <jacebaek06@gmail.com>
2025-06-15 02:45:17 -0600

    Made a rough layout of the whole calendar.

--- calendarapp/src/components/Flexbox.js
+++ calendarapp/src/components/Flexbox.js
@@ -1,10 +1,19 @@
 import "../components/Taskbar.css";
-import ".."
+import "../components/Calendar.css";
 
 function Taskbar() {
+    const days = Array.from({ length: 31 }, (_, i) => i + 1);
+
     return (
         <div className= "taskBody">
-        <div className= "taskbar"></div>
+            <div className= "taskbar"></div>
+         <div className="calBody">
+            {days.map((day) => (
+                <div key={day} className="calendar">
+                    {day}
+                </div>
+            ))}
+         </div>
         </div>
     )
 }
