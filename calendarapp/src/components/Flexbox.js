1f7ed2a63e43dd9a84add0b5263aaba2daa2a791
jacebaek1009 <jacebaek06@gmail.com>
2025-06-15 01:25:19 -0600

    making the overall layout of the app.

---
+++ calendarapp/src/components/Flexbox.js
@@ -0,0 +1,12 @@
+import "../components/Taskbar.css";
+import ".."
+
+function Taskbar() {
+    return (
+        <div className= "taskBody">
+        <div className= "taskbar"></div>
+        </div>
+    )
+}
+
+export default Taskbar;
